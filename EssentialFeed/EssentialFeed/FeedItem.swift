dc563a8ae58349bcb9042498820ccd9748c3d87b
Dejan Rakinic <dejanrakinic1304@gmail.com>
2022-10-19 23:52:58 +0200

    FeedItem model created

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Dejan Rakinic on 19.10.22..
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
