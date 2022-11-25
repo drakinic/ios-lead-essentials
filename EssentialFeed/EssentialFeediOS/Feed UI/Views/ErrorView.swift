f0658a25c6662d0c2f59fe30b4beab7381324b5d
Dejan Rakinic <dejan.rakinic@wm.rs>
2022-11-26 00:00:49 +0100

    Move ErrorView to separate file

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Views/ErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorView.swift
+//  EssentialFeediOS
+//
+//  Created by Dejan Rakinic on 25.11.22..
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
